c5831eaef1ca1cd30607ee6142f57540b874a901
lasse <77295879+lassv@users.noreply.github.com>
2022-08-29 10:37:40 +0200

    Update SocketContext.tsx

--- src/utils/context/SocketContext.tsx
+++ src/utils/context/SocketContext.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
 
-export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!, {
+export const socket = io("chat-platform-nestjs-production.up.railway.app"!, {
   withCredentials: true,
 });
 export const SocketContext = createContext(socket);
